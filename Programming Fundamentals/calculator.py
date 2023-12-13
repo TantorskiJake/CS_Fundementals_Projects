c383019fb4807aae14f14c38996550c9e2374bc7
Jake Tantorski <jake.tantorski1@marist.edu>
2023-12-13 08:06:41 -0500

    Removed redundancy and added comments

--- Programming Fundamentals/calculator.py
+++ Programming Fundamentals/calculator.py
@@ -1,27 +1,22 @@
-# Jake Tantorski December 13th, 2023
-
-# Import the math module for mathematical operations
+# Import the math module to use mathematical functions
 import math
 
-# Define a Calculator class to encapsulate calculator functionalities
+# Create a Calculator class to perform various mathematical operations
 class Calculator:
-    # Initialize the calculator with a memory variable set to None
     def __init__(self):
-        self.memory = None
+        self.memory = None  # Initialize memory attribute to store values
 
-    # Define method for addition operation
+    # Basic arithmetic operations
     def add(self, x, y):
         return x + y
 
-    # Define method for subtraction operation
     def subtract(self, x, y):
         return x - y
 
-    # Define method for multiplication operation
     def multiply(self, x, y):
         return x * y
 
-    # Define method for division operation with error handling
+    # Perform division with exception handling for division by zero
     def divide(self, x, y):
         try:
             result = x / y
@@ -29,108 +24,103 @@ class Calculator:
         except ZeroDivisionError:
             return "Cannot divide by zero"
 
-    # Define method for exponentiation operation
+    # Exponential function
     def exponentiate(self, x, y):
         return x ** y
 
-    # Define method for square root operation
+    # Square root function
     def square_root(self, x):
         return math.sqrt(x)
 
-    # Define method for sine operation
+    # Trigonometric functions, converting degrees to radians
     def sine(self, x):
         return math.sin(math.radians(x))
 
-    # Define method for cosine operation
     def cosine(self, x):
         return math.cos(math.radians(x))
 
-    # Define method for tangent operation
     def tangent(self, x):
         return math.tan(math.radians(x))
 
-    # Define method for logarithmic operation with input validation
+    # Logarithmic function with input validation
     def logarithm(self, x, base):
         if x > 0 and base > 1:
             return math.log(x, base)
         else:
             return "Invalid input for logarithm"
 
-    # Define method for storing a value in memory
+    # Store a value in memory
     def store_in_memory(self, value):
         self.memory = value
 
-    # Define method for recalling the value from memory with handling for empty memory
+    # Recall the value stored in memory
     def recall_memory(self):
         return self.memory if self.memory is not None else "Memory is empty"
-
 
 # Create an instance of the Calculator class
 calculator = Calculator()
 
-# Mapping choices to corresponding functions
+# Dictionary to map user input to corresponding calculator operations
 operations = {
-    '1': calculator.add,        # Addition
-    '2': calculator.subtract,   # Subtraction
-    '3': calculator.multiply,   # Multiplication
-    '4': calculator.divide,     # Division
-    '5': calculator.exponentiate,  # Exponentiation
-    '6': calculator.square_root,   # Square Root
-    '7': calculator.sine,       # Sine function
-    '8': calculator.cosine,     # Cosine function
-    '9': calculator.tangent,    # Tangent function
-    '10': calculator.logarithm  # Logarithmic function
+    '1': calculator.add,
+    '2': calculator.subtract,
+    '3': calculator.multiply,
+    '4': calculator.divide,
+    '5': calculator.exponentiate,
+    '6': calculator.square_root,
+    '7': calculator.sine,
+    '8': calculator.cosine,
+    '9': calculator.tangent,
+    '10': calculator.logarithm
 }
 
-# Define a function to get user input for numbers
+# Function to get numeric input from the user with exception handling
 def get_user_input():
     while True:
         try:
-            num1 = float(input("Enter the number: "))
-            return num1
+            return float(input("Enter the number: "))
         except ValueError:
             print("Invalid input. Please enter a numeric value.")
 
-# Define a function to display the result
+# Function to display the result with appropriate formatting
 def display_result(result):
-    # Use f-string for a more formatted and visually appealing result display
     print(f"Result: {result:.6f}" if isinstance(
         result, float) else f"Result: {result}")
 
-# Main program loop
+# Main loop for user interaction
 while True:
+    # Display available operations to the user
     print("\nSelect operation:")
-    # Print the available operations and their corresponding numbers
     for key, value in operations.items():
         print(f"{key}. {value.__name__.capitalize()}")
 
-    # Print additional options for storing and recalling memory
+    # Additional options for storing and recalling values in memory, and exiting the calculator
     print("11. Store in Memory")
     print("12. Recall Memory")
     print("13. Exit")
 
-    # Get user input for the selected operation
+    # Get user choice
     choice = input("Enter choice (1-13 or 'exit'): ")
 
-    # Check if the user chose to exit
+    # Check user choice and perform the corresponding operation
     if choice.lower() in ('13', 'exit'):
         print("Exiting the calculator. Goodbye!")
         break
 
-    # Check if the user choice is a valid operation
     if choice in operations:
         num1 = get_user_input()
 
+        # Special handling for square root and trigonometric functions that take a single input
         if choice in ('6', '7', '8', '9'):
-            # Execute the selected operation and display the result
             result = operations[choice](num1)
-            display_result(result)
         else:
             num2 = get_user_input()
-            # Execute the selected operation and display the result
             result = operations[choice](num1, num2)
-            display_result(result)
+
+        # Display the result to the user
+        display_result(result)
     elif choice == '11':
+        # Store a value in memory with input validation
         try:
             value_to_store = float(input("Enter value to store in memory: "))
             calculator.store_in_memory(value_to_store)
@@ -138,6 +128,7 @@ while True:
         except ValueError:
             print("Invalid input. Please enter a numeric value.")
     elif choice == '12':
+        # Recall the value from memory and display it to the user
         recalled_value = calculator.recall_memory()
         print("Recalled value from memory:", recalled_value)
     else:
